95ded3b4a55dfecef39de969977b5abb2ecfbcbc
wlp1993 <1902319500@qq.com>
2021-05-08 21:16:40 +0800

    xlua导入

---
+++ CindyPro/Assets/Scripts/login.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  XLua;
+public class login : MonoBehaviour
+{
+	private LuaEnv newluaenc;
+	// Use this for initialization
+	void Start ()
+	{
+		newluaenc = new LuaEnv();
+		newluaenc.DoString("print('ddddd')");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
